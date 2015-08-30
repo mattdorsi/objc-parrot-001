580d3014ee4ac5900613fc58bafd040b7214f187
mattdorsi <mdorsi4105@gmail.com>
2015-08-30 00:35:25 -0400

    new file:   objc-parrot/FISPerson.m

---
+++ objc-parrot/FISPerson.h
@@ -0,0 +1,13 @@
+//
+//  FISPerson.h
+//  objc-parrot
+//
+//  Created by Matt Dorsi on 8/30/15.
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISPerson : NSObject
+
+@end
---
+++ objc-parrot/FISPerson.m
@@ -0,0 +1,13 @@
+//
+//  FISPerson.m
+//  objc-parrot
+//
+//  Created by Matt Dorsi on 8/30/15.
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import "FISPerson.h"
+
+@implementation FISPerson
+
+@end
